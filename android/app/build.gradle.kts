779eda7dfd6c0f6e4dabdd129ce4a922207c5cae
Justin Zero <justinezero888@gmail.com>
2026-06-10 15:25:54 -0400

    refactor: unify bundle ID to com.orbacetech.stalio
    Before (inconsistent):
    - Android: com.microhabits.micro_habits
    - iOS:     com.microhabits.microHabits
    - macOS:   com.microhabits.microHabits (micro_habits.app)
    
    After (consistent):
    - All platforms: com.orbacetech.stalio
    
    Changed files:
    - android/app/build.gradle.kts (namespace + applicationId)
    - ios/Runner.xcodeproj/project.pbxproj (PRODUCT_BUNDLE_IDENTIFIER, 6 occ)
    - macos/Runner.xcodeproj/project.pbxproj (bundle ID + app name)
    - test/maestro/*.yaml (appId, 8 files)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.microhabits.micro_habits"
+    namespace = "com.orbacetech.stalio"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.microhabits.micro_habits"
+        applicationId = "com.orbacetech.stalio"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
